cc908c77796da01784b4b935d804af73f40efc93
S.K <cecilia.mcszk@gmail.com>
2022-04-21 22:39:00 +0200

    add final animation for menu

--- assets/js/menu-chart.js
+++ assets/js/menu-chart.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
   })
 });
 
+// TODO screamer random circle click
 // Draw Chart
 function drawChart(data){
   // CONST
@@ -45,7 +46,8 @@ function drawChart(data){
   // VARS
   const colors = {
     lightPurple: '#CDB2E8',
-    lightGray: '#D1CCD6'
+    lightGray: '#D1CCD6',
+    white: '#FFF'
   }
   const name = 'SK'
 
@@ -73,28 +75,30 @@ function drawChart(data){
                          .attr('cy', y(0.5))
                          .attr('r', innerRadius)
                          .style('fill', 'none')
-                         .style('stroke', 'none')
-                         .style('stroke-dasharray', 4)
-
-
-
-
+                         .style('stroke', 'none');
 
   // Simulation with radial force
   const simulation = d3.forceSimulation(nodes)
                        .force("collide", d3.forceCollide(9))
                        .force("radial", d3.forceRadial(r, x(0.5), y(0.5)))
                        .stop()
-                       .tick(ticked)
+                       .tick(ticked);
+
+  const groupMenu = svg.append('g').classed('group-menu', true).attr('id', 'group-menu');
+  const groupText = svg.append('g').attr('id', 'group-text');
+
+
+  // LANG VAR
+  let lang = 'en'; // default is english TODO const
 
   // initNodedArray()
   // Init data
   // Init array of objects [id (float), x (float), y (float), r (float)]
   // return Array of Objects
-  function initNodedArray(){
+  function initNodedArray() {
     // init empty array
     let arrayOfObj = [];
-    for(let idx = 0; idx < 100; idx++){
+    for(let idx = 0; idx < 100; idx++) {
       let obj = {
         id : idx,
         x : Math.random()*100,
@@ -106,12 +110,49 @@ function drawChart(data){
     return arrayOfObj;
   }
 
+  // getMultiLangText
+  // returns Array of objects
+  // args
+  // startId : int, index of the first el
+  // endId : int, index of the last el
+  function getMultiLangText(startId, endId) {
+    let arrayOfTextObj = []
+    for(let indexOfArray = startId; indexOfArray <= endId; indexOfArray++){
+      switch (lang) {
+        case 'fr':
+          arrayOfTextObj.push(langData[indexOfArray].fr)
+          break;
+        case 'jp':
+          arrayOfTextObj.push(langData[indexOfArray].jp)
+          break;
+        default:
+          arrayOfTextObj.push(langData[indexOfArray].en) //default en
+      }
+    }
+    return arrayOfTextObj;
+  }
+
+  // appendMultipleText
+  // append multiple svg text depeding on the size of the dataset (array)
+  // args
+  // dataArray: Array of strings
+  // class: string - class to append to the element
+  function appendMultipleText(dataArray, classText) {
+    dataArray.map(function(d, i) {
+      groupText.append('text')
+          .text(dataArray[i])
+          .classed(classText, true)
+          .attr('id', 'menu-text-'+ i)
+          .style('fill', colors.lightGray);
+    })
+  }
+
   // Draw nodes
   // args
   // svg = SVG DOM el
   // data = dataset (array of Objects)
   // return node array of obj
-  function drawNodes(svg, data){
+  function drawNodes(svg, data) {
 
     // getRandomInt
     // function that returns a random Int
@@ -136,54 +177,70 @@ function drawChart(data){
                             .padAngle(.01)
                             .sort(null);
 
-    let circle = svg.append('g')
-                    .classed('group-menu', true)
-                    .selectAll('.node')
-                    .data(circleGuidePie(data)).enter()
-                    .append('circle')
-                      .style('fill', colors.lightPurple)
-                      .style('opacity', 0.5)
-                      .classed('node', true)
-                      .attr('class', function(d, i){
-                        if(i == 99 || i == 33 || i == 67) {
-                          return 'circle-menu'
-                        }
-                        else {
-                          //Get random number for natural animation
-                          let rdmNumb = getRandomInt(11)
-                          return 'circle-anim-'+ rdmNumb
-                        }
-                      })
-                      .attr('cx', function(d){
-                        let centroid = arc.centroid(d)
-                        return centroid[0] + x(0.5)
-                      })
-                      .attr('cy', function(d){
-                        let centroid = arc.centroid(d)
+    let circle = groupMenu.selectAll('.node')
+                          .data(circleGuidePie(data)).enter()
+                          .append('circle')
+                          .style('fill', colors.lightPurple)
+                          .style('opacity', 0.5)
+                          .classed('node', true)
+                          .attr('class', function(d, i) {
+                          // index being the 3 categories of menu
+                            if(i == 99 || i == 33 || i == 67) {
+                              return 'circle-menu';
+                            }
+                           else {
+                            //Get random number for natural animation
+                            let rdmNumb = getRandomInt(11)
+                            return 'circle-anim-'+ rdmNumb
+                          }
+                        })
+                        // .attr('id', function(d, i) {
+                        //   return 'circle-' + i;
+                        // })
+                        .attr('cx', function(d){
+                          let centroid = arc.centroid(d)
+                          return centroid[0] + x(0.5)
+                        })
+                        .attr('cy', function(d){
+                          let centroid = arc.centroid(d)
                           return centroid[1] + y(0.5)
                         })
-                      .attr('r', function(d, i){
-                        // iterate through nodeData since d is configured for pie
-                        return nodeData[i].r * 1.5;
-                      });
+                        .attr('r', function(d, i){
+                          // iterate through nodeData since d is configured for pie
+                          return nodeData[i].r * 1.5;
+                        });
 
 
-    d3.selectAll('.circle-menu')
-      .attr('r', 50);
+
+    d3.selectAll('.circle-menu').attr('r', 50);
+
+    //Append ID to menu circles
+    svg.selectAll('.circle-menu')
+       .attr('id', function(d,i) {
+         return 'circle-menu-' + i
+       })
 
     // Animations classes
-    circle.on('mouseover', function(d, i) {
-      console.log(i.index)
+    circle.on('mouseover', function() {
       d3.select(this).classed('circle-hover', true);
       if(d3.select(this).attr('class') == 'circle-menu circle-hover'){
+        let circleID = d3.select(this).node().getAttribute('id');
+        let id = circleID.split('-')[circleID.split('-').length - 1]
+        d3.select('#menu-text-' + id).style('opacity', '1');
         d3.select(this).attr('r', 100);
+        d3.select(this).style('opacity', 0.2);
       }
     })
     circle.on('mouseout', function() {
       d3.select(this).classed('circle-hover', false);
       if(d3.select(this).attr('class') == 'circle-menu'){
+        // reset display
+        let circleID = d3.select(this).node().getAttribute('id');
+        let id = circleID.split('-')[circleID.split('-').length - 1]
+        d3.select('#menu-text-' + id).style('opacity', '0');
         // Reset r
         d3.select(this).attr('r', 50);
+        d3.select(this).style('opacity', 0.5);
       }
     })
   }
@@ -209,9 +266,12 @@ function drawChart(data){
        .attr('y', (y(0.5)));
 
   let subTitle = svg.append('text')
-                    .text(data[0].en)
+                    .text(langData[0].en)
                     .classed('subtitle', true)
-                    .style('fill', colors.lightGray);
+                    .style('fill', colors.white);
+
+  let menuText = getMultiLangText(1, 3);
+  appendMultipleText(menuText, 'menu-text');
 
   let subtitleSelect = d3.select('.subtitle').node();
   let subtitleW = subtitleSelect.getBoundingClientRect().width;
@@ -219,6 +279,40 @@ function drawChart(data){
   subTitle.attr('x', (x(0.5)) - subtitleW / 2)
           .attr('y', (y(0.5) + chartParam.margin.top * 2.5));
 
+
   // For testing purpose
   drawNodes(svg, nodes);
+
+
+  // Place menu elements
+  // /!\ IDS and classes are hardcoded
+  let textParam = {
+    menu1: {
+      x: d3.select('#circle-menu-0').node().getAttribute('cx'),
+      y: d3.select('#circle-menu-0').node().getAttribute('cy'),
+      textW: svg.selectAll('#menu-text-0').node().getBoundingClientRect().width,
+      textH: svg.selectAll('#menu-text-0').node().getBoundingClientRect().height
+    },
+    menu2: {
+      x: d3.select('#circle-menu-1').node().getAttribute('cx'),
+      y: d3.select('#circle-menu-1').node().getAttribute('cy'),
+      textW: svg.selectAll('#menu-text-1').node().getBoundingClientRect().width,
+      textH: svg.selectAll('#menu-text-1').node().getBoundingClientRect().height
+    },
+    menu3: {
+      x: d3.select('#circle-menu-2').node().getAttribute('cx'),
+      y: d3.select('#circle-menu-2').node().getAttribute('cy'),
+      textW: svg.selectAll('#menu-text-2').node().getBoundingClientRect().width,
+      textH: svg.selectAll('#menu-text-2').node().getBoundingClientRect().height
+    }
+  }
+  Object.entries(textParam).map(function(item, i) {
+    let selectedTextMenu = svg.select('#menu-text-'+i);
+    selectedTextMenu.attr('x', item[1].x - (item[1].textW / 2))
+                    .attr('y', item[1].y);
+  })
+
+
+  // Display text of menu
+
 }
